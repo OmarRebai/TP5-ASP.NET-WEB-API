b0a23d0d7fe1109943f2292266e29d0b8ad02176
omar <omar.rebai@enis.tn>
2023-12-12 01:46:07 +0100

    COMPLETEDAjoutez des fichiers projet.

---
+++ Orders.cs
@@ -0,0 +1,10 @@
+﻿namespace OrdersAPI
+{
+    public class Orders
+    {
+        public int OrdersId { get; set; }
+        public DateTime OrdersDate { get; set; }
+        public string? CustomerName { get; set; }
+        public int ProductId { get; set; }
+    }
+}
